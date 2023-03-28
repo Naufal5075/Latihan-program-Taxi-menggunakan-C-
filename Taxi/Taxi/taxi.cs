d7374372a86017a03d72f5cab6a712f3a147c96c
Naufal5075 <128124617+Naufal5075@users.noreply.github.com>
2023-03-29 05:07:49 +0700

    Add files via upload

---
+++ Taxi/Taxi/taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanTaxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            if (OnDuty)
+            {
+                Console.WriteLine("On Duty: Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+
+        }
+
+        public void PickUpPassenger()
+       {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+       }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang", DriverName);
+        }
+    }
+}
